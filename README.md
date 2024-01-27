87a6ce73aa3dac99b7700bb9f46a06107d064ff4
brgaulin <4007825+brgaulin@users.noreply.github.com>
2024-01-27 00:32:44 -0500

    Deploy with CDK v2, Node 20

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# stadium-events
+# Stadium Events
 An iCal parser that takes in the iCal file from your local stadium / arena, and converts it so you get alerted for upcoming traffic
+
+# Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
+
+# Local Setup
+
+```bash
+npm install -g aws-cdk typescript
+npm install
+npm run build
+```
+
+# Deployment
+
+```bash
+cdk deploy
+```
+
+# CDK Development Notes
+
+NodeJS Lambda Function: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html
+
+First Time Setup `cdk bootstrap ...`
---
+++ lib/stadium-events.ts
@@ -0,0 +1,33 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import events = require('aws-cdk-lib/aws-events');
+import targets = require('aws-cdk-lib/aws-events-targets');
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import iam = require('aws-cdk-lib/aws-iam');
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+
+export class StadiumEventsLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Lambda Function
+    const lambdaFn = new NodejsFunction(this, 'main', {
+      runtime: Runtime.NODEJS_20_X ,
+      timeout: cdk.Duration.seconds(300)
+    })
+
+    // Run the eventbridge every week
+    const rule = new events.Rule(this, 'WeeklyCron', {
+      schedule: events.Schedule.expression('cron(0 6 ? * 2 *)')
+    });
+
+    rule.addTarget(new targets.LambdaFunction(lambdaFn));
+
+    const s3PutPolicy = new iam.PolicyStatement({
+      actions: ['s3:PutObject', 's3:PutObjectAcl'],
+      resources: ['arn:aws:s3:::simple-gillette-calendar/events.ics'],
+    });
+
+    lambdaFn.addToRolePolicy(s3PutPolicy);
+  }
+}
